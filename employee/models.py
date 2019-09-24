37a0250a68e3a2112cfc37580f7918dc63b7f278
Dara <Anil-R.Dara@shell.com>
2019-09-24 17:20:08 +0530

    modelchanges

--- employee/models.py
+++ employee/models.py
@@ -2,13 +2,6 @@ from mongoengine import Document, EmbeddedDocument, fields
 
 # Create your models here.
 
-class Employee(Document):
-    empId = fields.StringField(max_length=10, required=True, null=False)
-    empName = fields.StringField(max_length=100, required = True)
-    workLocation = fields.StringField(max_length=100, required =False)
-    projects = fields.EmbeddedDocumentListField(Project)
-    skills = fields.EmbeddedDocumentListField(Skill)
-
 class Project(EmbeddedDocument):
     projectId = fields.StringField(max_length=10, required=True, null=False)
     projectName = fields.StringField(max_length=100, required=True)
@@ -21,3 +14,10 @@ class Skills(EmbeddedDocument):
     experience = fields.IntField(min_value=2,max_value=25,required=True)
     level = fields.StringField(max_length=27, required=True)
 
+
+class Employee(Document):
+    empId = fields.StringField(max_length=10, required=True, null=False)
+    empName = fields.StringField(max_length=100, required = True)
+    workLocation = fields.StringField(max_length=100, required =False)
+    projects = fields.EmbeddedDocumentListField(Project)
+    skills = fields.EmbeddedDocumentListField(Skills)
